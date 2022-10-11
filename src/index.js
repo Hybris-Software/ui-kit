0331532e8c281f521acf708c46e72597869c5e2f
Biagio Nuzzo <57946383+biagio-nuzzo@users.noreply.github.com>
2022-10-11 16:20:29 +0400

    fixed import

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import Button from "./Components/Button/Button";
-import Col from "./Components/Col/Col";
 import Container from "./Components/Container/Container";
-import InputField from "./Components/InputField/InputField";
 import Row from "./Components/Row/Row";
+import Col from "./Components/Col/Col";
+import Button from "./Components/Button/Button";
+import InputField from "./Components/InputField/InputField";
 
 export { Button, Col, Container, InputField, Row };
