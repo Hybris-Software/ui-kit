4fabfd976221aa0fa1c410523543ddffb3cf5b8d
Biagio Nuzzo <57946383+biagio-nuzzo@users.noreply.github.com>
2022-10-11 16:33:29 +0400

    run build

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
-import Button from "./Components/Button/Button";
-import InputField from "./Components/InputField/InputField";
-import Col from "./Components/GridBasic/Col";
 import Container from "./Components/GridBasic/Container";
 import Row from "./Components/GridBasic/Row";
+import Col from "./Components/GridBasic/Col";
+import Button from "./Components/Button/Button";
+import InputField from "./Components/InputField/InputField";
 
 export { Button, Col, Container, InputField, Row };
