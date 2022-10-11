18af185ef63a1f91eb7fe1c118841e71b3c8f473
Biagio Nuzzo <57946383+biagio-nuzzo@users.noreply.github.com>
2022-10-11 17:35:58 +0400

    fixed orderLg

--- src/Components/GridBasic/Col.jsx
+++ src/Components/GridBasic/Col.jsx
@@ -1,41 +1,62 @@
-import React from 'react'
+import React from "react";
 
 // Utils
-import classNames from '../../Utils/classNames'
+import classNames from "../../Utils/classNames";
 
 // Styles
-import Style from './GridBasic.module.css'
+import Style from "./GridBasic.module.css";
 
-const Col = ({ sm, md, lg, xl, xxl, offsetSm, offsetMd, offsetLg, offsetXl, offsetXxl, orderSm, orderMd, orderlg, orderXl, orderXxl, className, style, children }) => {
-    return (
-        <div
-            className={classNames(
-                [!sm, !md, !lg, !xl, !xxl].includes(false) && !className ? ('').trim : Style.col,
-                sm && Style[`col-sm-${sm}`],
-                md && Style[`col-md-${md}`],
-                lg && Style[`col-lg-${lg}`],
-                xl && Style[`col-xl-${xl}`],
-                xxl && Style[`col-xxl-${xxl}`],
-                offsetSm && Style[`offset-sm-${offsetSm}`],
-                offsetMd && Style[`offset-md-${offsetMd}`],
-                offsetLg && Style[`offset-lg-${offsetLg}`],
-                offsetXl && Style[`offset-xl-${offsetXl}`],
-                offsetXxl && Style[`offset-xxl-${offsetXxl}`],
-                orderSm && Style[`order-sm-${orderSm}`],
-                orderMd && Style[`order-md-${orderMd}`],
-                orderlg && Style[`order-lg-${orderlg}`],
-                orderXl && Style[`order-xl-${orderXl}`],
-                orderXxl && Style[`order-xxl-${orderXxl}`],
-                className
-            )}
-            style={style}
-        >
-            {children}
-        </div>
-    )
-}
+const Col = ({
+  sm,
+  md,
+  lg,
+  xl,
+  xxl,
+  offsetSm,
+  offsetMd,
+  offsetLg,
+  offsetXl,
+  offsetXxl,
+  orderSm,
+  orderMd,
+  orderLg,
+  orderXl,
+  orderXxl,
+  className,
+  style,
+  children,
+}) => {
+  return (
+    <div
+      className={classNames(
+        [!sm, !md, !lg, !xl, !xxl].includes(false) && !className
+          ? "".trim
+          : Style.col,
+        sm && Style[`col-sm-${sm}`],
+        md && Style[`col-md-${md}`],
+        lg && Style[`col-lg-${lg}`],
+        xl && Style[`col-xl-${xl}`],
+        xxl && Style[`col-xxl-${xxl}`],
+        offsetSm && Style[`offset-sm-${offsetSm}`],
+        offsetMd && Style[`offset-md-${offsetMd}`],
+        offsetLg && Style[`offset-lg-${offsetLg}`],
+        offsetXl && Style[`offset-xl-${offsetXl}`],
+        offsetXxl && Style[`offset-xxl-${offsetXxl}`],
+        orderSm && Style[`order-sm-${orderSm}`],
+        orderMd && Style[`order-md-${orderMd}`],
+        orderLg && Style[`order-lg-${orderlg}`],
+        orderXl && Style[`order-xl-${orderXl}`],
+        orderXxl && Style[`order-xxl-${orderXxl}`],
+        className
+      )}
+      style={style}
+    >
+      {children}
+    </div>
+  );
+};
 
-export default Col
+export default Col;
 
 // Language: javascript
 // # sm: Set the width of the column for small screens (1 to 12)
